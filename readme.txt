5ea0a238e1a94bc4ce0c7922159ffab4f18ff26f
mikeselander <mikeselander@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-04 00:47:11 +0000

    Add 0.2.0 files to tags
    git-svn-id: https://plugins.svn.wordpress.org/simple-frontend-template-display/tags/0.2.0@814713 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,52 @@
+=== Simple Frontend Template Display ===
+Contributors: mikeselander
+Donate link: http://mikeselander.com
+Tags: template name, admin bar, front end display, page template, development, view, page, toolbar
+Requires at least: 3.4
+Tested up to: 3.7.1
+Stable tag: 0.2.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds an admin toolbar menu item that displays the current page template.
+
+== Description ==
+
+Simple Frontend Template Display will show the name of the template that you’re using in the toolbar when you’re in the frontend of your website. You can use this to keep track of the various templates you’re using without having to hop into the edit screen every time you want to check a template.
+
+All you have to do to get this working is to install the plugin. It will then automatically show up in the toolbar on all pages on your site. 
+
+I hope this speeds up your development work!
+
+== Installation ===
+
+1. Upload `Simple Frontend Template Display` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Magic! On your pages, the name of the template will show on the toolbar on the fronted
+
+== Frequently Asked Questions ==
+
+= What do I have to do to get this working? =
+
+Install the plugin!
+
+It’s super easy and requires almost no work on your part. It will simply start showing up once you have it all installed.
+
+== Upgrade Notice ==
+
+= 0.2.0 =
+Adds a drop down item to show the page template filename & improves several methods
+
+= 0.1.0 =
+Initial Release
+
+== Changelog ==
+
+= 0.2.0 =
+* Adapted add_menu method to the preferred add_node
+* Fixed several typographical & commenting errors
+* Split up the filename and template name functions to allow for the separate dropdown
+* Added a drop down with the filename so you can see all of the template data
+
+= 0.1.0 =
+* Initial release
---
+++ simple-frontend-template-display.php
@@ -0,0 +1,130 @@
+<?php
+defined( 'ABSPATH' ) OR exit;
+/*
+Plugin Name: Simple Frontend Template Display
+Plugin URI: http://www.mikeselander.com/
+Description: Displays the current page template in the admin bar for quick & easy reference
+Version: 0.2
+Author: Mike Selander
+Author URI: http://www.mikeselander.com/
+License: GPL2
+*/
+
+/**
+ * PageTemplateDisplay
+ *
+ * Creates a section on the admin menu bar to see the current page template
+ *
+ * @package WordPress
+ * @category plugins
+ * @author Mike Selander
+ * @since 0.0.0
+ * @link      http://mikeselander.com
+ * @copyright 2013 Mike Selander
+ */
+class PageTemplateDisplay{
+
+	const VERSION = '0.2.0';
+
+	/**
+	 * Constructor function.
+	 *
+	 * @access public
+	 * @since 0.1.0
+	 * @return void
+	 */
+	public function __construct(){
+
+		add_action('admin_bar_menu', array( $this, "page_template_display" ), 500 );
+
+	} // end __construct()
+
+	/**
+	 * Displays the template name on the admin menu bar
+	 *
+	 * @access public
+	 * @since 0.1.0
+	 */
+	public function page_template_display( $wp_admin_bar ){
+		//global $wp_admin_bar;
+
+		if (!is_super_admin() || !is_admin_bar_showing() )
+			return;
+
+		// if you're not in the admin backend & it's a page
+		if ( ( !is_admin() ) && ( is_page() ) ){
+
+			// if get_current_template_name returns - set the menu item
+			if ( $this->get_current_template_name() ) {
+				$wp_admin_bar->add_node(
+					array(
+						'id' 		=> 'page_template',
+						'title' 	=> __( 'Template: '.$this->get_current_template_name(), 'page_template' ),
+						'parent'	=> false,
+						'href' 		=> false
+					)
+				);
+				$wp_admin_bar->add_node(
+					array(
+						'id' 		=> 'page_template_slug',
+						'title' 	=> __( $this->get_current_page_slug(), 'page_template' ),
+						'parent'	=> page_template,
+						'href' 		=> false
+					)
+				);
+
+
+			} // end if template name is returned
+
+		} // end if !is_admin && is_page
+
+	} // end page_template_display()
+
+	/**
+	 * Grabs the template name
+	 *
+	 * @access public
+	 * @since 0.1.0
+	 * @return Template name string
+	 */
+	public function get_current_template_name(){
+
+		// get all templates
+		$all_templates = wp_get_theme()->get_page_templates();
+		$returned = false;
+		foreach ( $all_templates as $filename => $name ) {
+
+			// if the template matches one of the filenames, return
+			if ( $this->get_current_page_slug() == $filename ){
+				return $name;
+				$returned = true;
+			};
+
+		} // end foreach
+
+		// If no slug is returned, then return "Default"
+		if ( $returned === false ){
+			return "Default";
+		}
+
+	} // end get_current_template_name()
+
+	/**
+	 * Grabs the template slug
+	 *
+	 * @access public
+	 * @since 0.2.0
+	 * @return Template slug string
+	 */
+	public function get_current_page_slug(){
+		global $wp_query;
+
+		$post_id = $wp_query->post->ID;
+		return get_page_template_slug($post_id);
+	} // end get_current_page_slug()
+
+} // end PageTemplateDisplay
+
+$display_template = new PageTemplateDisplay;
+
+?>
